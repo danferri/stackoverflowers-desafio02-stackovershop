ce2c41ee80daba1eba38bb68aa9d8607dd065996
Daniel Ferri <89649179+danferri@users.noreply.github.com>
2024-12-30 18:09:31 -0300

    feat: finalize implementation of validatePlants.ts middleware.

--- stackovershop-api/src/middlewares/validatePlants.ts
+++ stackovershop-api/src/middlewares/validatePlants.ts
@@ -1,22 +1,37 @@
+import { FastifyReply, FastifyRequest } from "fastify";
 import * as yup from "yup";
+import prismaClient from "../database/connection";
 
 const plantSchema = yup.object().shape({
     name: yup.string().required("The name is required."),
     subtitle: yup.string().required("Subtitle is required."),
+    labels: yup.array().of(yup.string()).required("Labels are required."),
     price: yup
         .number()
         .typeError("The price must be a number.")
         .positive()
         .required("The price is required."),
-    typeId: yup
-        .number()
-        .integer("The typeId must be a integer.")
-        .required("The typeId is required"),
-    description: yup.string().required("The description is required."),
     isInSale: yup.boolean().optional(),
     disccountPercentage: yup
         .number()
         .min(0, "Discount must be at least 0.")
         .max(100, "Discount canot exceed 100.")
-        .optional(), 
+        .optional(),
+    description: yup.string().required("The description is required."),
 });
+
+export const validatePlant = async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+        await plantSchema.validate(request.body, { abortEarly: false });
+
+    } catch (error) {
+        if (error instanceof yup.ValidationError) {
+            const errors = error.inner.map((err) => ({
+                path: err.path,
+                message: err.message,
+            }));
+            return reply.status(400).send({ errors });
+        }
+        return reply.status(500).send({ error: "Unexpected error during validation." });
+    }
+};
