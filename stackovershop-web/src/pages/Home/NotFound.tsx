2de8ea8fe378ab59020f3c9dc6383c45b5d7d520
Caue Campos <cauewc05@gmail.com>
2025-01-02 19:17:33 -0300

    feat: first component of home page was created

---
+++ stackovershop-web/src/pages/Home/NotFound.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ShopNow from '../../components/ShopNow';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <ShopNow />
+    </div>
+  );
+};
+
+export default Home;
