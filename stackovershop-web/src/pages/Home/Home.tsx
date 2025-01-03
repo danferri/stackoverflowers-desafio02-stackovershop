0fd586cd30952069fc03a7cf5e61e867e82ef4ea
Pnonato <like.nonato.marrara@gmail.com>
2025-01-03 16:08:17 -0300

    feat: splide creation

--- stackovershop-web/src/pages/Home/Home.tsx
+++ stackovershop-web/src/pages/Home/Home.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ShopNow from '../../components/ShopNow';
 import TakeCare from '../../components/StepsToTakeCare';
+import Slides from '../../components/component/Splide/Slides'
+import StaticSlides from '../../components/component/Splide/StaticSlide'
+import Style from '../../components/component/Splide/mySlider.css'
+
 
 const Home: React.FC = () => {
   return (
     <div>
       <ShopNow />
       <TakeCare />
+      <Slides/>
+      <h1 className="center-title">Plant In Sale</h1>
+      <StaticSlides/>
+      
     </div>
   );
 };
