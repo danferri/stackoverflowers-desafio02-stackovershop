688283d3acb1a092c33844356028fc42cca64ed2
Arthur.s08 <arthur.sbruzzi@gmail.com>
2025-01-03 20:41:46 -0300

    style: made product styles css

--- stackovershop-web/src/components/Products.tsx
+++ stackovershop-web/src/components/Products.tsx
@@ -43,39 +43,41 @@ const Products = () => {
     
     return (
       <div className='products'>
-          {plantas.map((plant, id) => (
-            <div key={id} className='PlantInfo'>
+          {/* {plantas.map((plant, id) => ( */}
+
+          {/* Imagem */}
+          <img src="https://cdn.awsli.com.br/2500x2500/2441/2441441/produto/247945060/6d433d62-0af2-4d1e-987c-c793c7cceffb-6jdf614gfd.jpeg" alt="cacto" className='image' />
+          
+            <div className='PlantInfo'>
             {/* Título e Subtítulo */}
             <div className='plantTitle'>
-                <h3 className='plantName'>{plant.name}</h3>
-                <p className='plantSub'>{plant.subtitle}</p>
+                <h3 className='plantName'>Cacto</h3>
+                <p className='plantSub'>Lindo cheiroso (nem da pra ser cheiroso)</p>
             </div>
 
             {/* Labels e Tipo de Planta */}
             <div className='plantLabels'>
-                {plant.labels.map((label, id) => (
-                    <p key={id} className='plantLabel'>{label}</p>
-                ))}
-                <p className='plantType'>{plant.plantType} planta</p>
+                {/* {plant.labels.map((label, id) => ( */}
+                    <p  className='plantLabel'>grande</p>
+                {/* ))} */}
+                <p className='plantType'>indoor</p>
             </div>
 
             {/* Preço e Botão */}
-            <p className='plantPrice'>$ {plant.price.toFixed(2)}</p>
+            <p className='plantPrice'>$ 10</p>
             <button>Check out</button>
 
             {/* Features e Descrição */}
             <p className='features'>Features</p>
-            <p className='plantFeatures'>{plant.features}</p>
-
+            <ul>
+            <li className='plantFeatures'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus laboriosam eum libero architecto quaerat accusamus dolorum omnis odit, quidem dignissimos aliquid et rem minus vitae accusantium, quos quibusdam exercitationem vero.</li>
+            </ul>
             <div className='plantDescription'>
                 <p className='description'>Description</p>
-                <p className='plantDescriptions'>{plant.description}</p>
+                <p className='plantDescriptions'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis unde illo et aliquid numquam quod veniam eos asperiores sed voluptates doloribus, velit officiis at minus inventore modi maiores quisquam?</p>
             </div>
-
-            {/* Imagem */}
-            <img src={plant.imgUrl} alt={plant.name} className='image' />
-        </div>
-          ))}
+            </div>
+          {/* ))} */}
       </div>
     );
   };
