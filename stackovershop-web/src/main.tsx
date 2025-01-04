5c464d4f78bd11c7070b036dd72f61a224095bbb
Caue Campos <cauewc05@gmail.com>
2025-01-04 10:53:08 -0300

    feat: implement header on homepage

--- stackovershop-web/src/main.tsx
+++ stackovershop-web/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; 
 import AppRoutes from './routes';
+import { ClerkProvider } from '@clerk/clerk-react';
 
 const container = document.getElementById('root');
 
@@ -8,7 +9,9 @@ if (container) {
   const root = ReactDOM.createRoot(container); 
   root.render(
     <React.StrictMode>
+       <ClerkProvider publishableKey={"pk_test_bmV1dHJhbC1jb3VnYXItNTYuY2xlcmsuYWNjb3VudHMuZGV2JA"} afterSignOutUrl="/">
       <AppRoutes />
+      </ClerkProvider>
     </React.StrictMode>
   );
 }
--- stackovershop-web/src/pages/Home/Home.tsx
+++ stackovershop-web/src/pages/Home/Home.tsx
@@ -4,11 +4,13 @@ import TakeCare from '../../components/StepsToTakeCare';
 import Slides from '../../components/component/Splide/Slides'
 import StaticSlides from '../../components/component/Splide/StaticSlide'
 import Style from '../../components/component/Splide/mySlider.css'
+import Header from '../../components/component/Header/Header';
 
 
 const Home: React.FC = () => {
   return (
     <div>
+      <Header />
       <ShopNow />
       <TakeCare />
       <h1 className="heading-text" id='carrossel1'>
