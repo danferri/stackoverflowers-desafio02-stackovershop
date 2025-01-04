b968b6b6d25042e2eeeed89dfe8638841845c3bc
Amanda Amabili <amandaamabilidasilva100@gmail.com>
2025-01-04 00:10:41 -0300

    form css

---
+++ stackovershop-web/src/pages/Form/FormPlant.tsx
@@ -0,0 +1,15 @@
+import Form from '../../components/Form';
+import './form.css';
+
+
+const FormPlant = () => {
+  return (
+    <article>
+    
+        <Form onSuccess={() => {}} />
+          
+    </article>
+  )
+}
+
+export default FormPlant
--- stackovershop-web/src/routes.tsx
+++ stackovershop-web/src/routes.tsx
@@ -2,14 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AboutUs from './pages/AboutUs/AboutUs';
 import PageNotFound from './pages/NotFound/NotFound';
 import Home from './pages/Home/Home';
+import FormPlant  from "../src/components/Form"; 
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="*" element={<PageNotFound />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/form" element={<FormPlant onSuccess={() => {}} />} />
       </Routes>
     </BrowserRouter>
   );
