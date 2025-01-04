0904599ab9cc1cff27fb310e2c65e5e783bfc4a6
Daniel Ferri <89649179+danferri@users.noreply.github.com>
2025-01-04 18:52:18 -0300

    refactor: plant types validation  refactor.

--- stackovershop-api/src/middlewares/validatePlantTypes.ts
+++ stackovershop-api/src/middlewares/validatePlantTypes.ts
@@ -4,9 +4,44 @@ import prismaClient from "../database/connection";
 import { ObjectId } from "mongodb";
 
 const plantTypeSchema = yup.object().shape({
-    plantTypeId: yup.string().required("Plant type id is required."),
+  plantType: yup
+    .string()
+    .required("Plant type is required.")
+    .min(3, "Plant type must have at least 3 characters."),
 });
 
-export const validatePlantTypes = async (request: FastifyRequest, reply: FastifyReply) => {
-  
+export const validatePlantTypes = async (
+  request: FastifyRequest,
+  reply: FastifyReply
+) => {
+  try {    
+    await plantTypeSchema.validate(request.body, { abortEarly: false });
+
+    const { plantType } = request.body as { plantType: string };
+
+    const plantTypeExists = await prismaClient.plantType.findFirst({
+      where: { plantType },
+    });
+
+    if (!plantTypeExists) {
+      return reply
+        .status(404)
+        .send({ error: "Plant type does not exist in the database." });
+    }
+   
+    return reply.status(200).send({ message: "Plant type is valid." });
+  } catch (error) {
+    if (error instanceof yup.ValidationError) {
+      
+      return reply.status(400).send({
+        error: "Validation Error",
+        details: error.errors,
+      });
+    }
+    
+    return reply.status(500).send({
+      error: "Internal Server Error",
+      details: (error as any).message,
+    });
+  }
 };
