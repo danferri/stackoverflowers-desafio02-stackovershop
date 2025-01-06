9f9d9a4112895326b14becd885d487142452b72c
Caue Campos <cauewc05@gmail.com>
2025-01-05 23:20:09 -0300

    style: css product page

--- stackovershop-web/src/components/Products.tsx
+++ stackovershop-web/src/components/Products.tsx
@@ -23,8 +23,9 @@ interface Plant {
 
 const Products = () => {
   const { id } = useParams();
-  const [plant, setPlant] = useState<Plant | null>();
-  const [error, setError] = useState(null);
+  const [plant, setPlant] = useState<Plant | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
   const images = [cacto, cacto2, image3, image4, imgDefault];
   const randomIndex = Math.floor(Math.random() * images.length);
 
@@ -39,40 +40,33 @@ const Products = () => {
     };
 
     fetchData();
-  }, []);
+  }, [id]);
 
   if (error) return <p>Erro: {error}</p>;
 
   return (
     <div className="products">
       {plant && (
-        <div>
+        <>
           <div className="PlantImage">
-            {/* Imagem */}
+            {/* Imagem da planta */}
             <img src={images[randomIndex]} alt={plant.name} className="image" />
           </div>
           <div className="PlantInfo">
-            {/* Título e Subtítulo */}
+            {/* Informações da planta */}
             <div className="plantTitle">
               <h3 className="plantName">{plant.name}</h3>
               <p className="plantSub">{plant.subtitle}</p>
             </div>
-
-            {/* Labels e Tipo de Planta */}
             <div className="plantLabels">
-              {plant.labels.map((label: string, id: number) => (
-                <p key={id} className="plantLabel">
+              {plant.labels.map((label, index) => (
+                <p key={index} className="plantLabel">
                   {label}
                 </p>
               ))}
-              <p className="plantType">{plant.plantType}</p>
             </div>
-
-            {/* Preço e Botão */}
             <p className="plantPrice">${plant.price}</p>
-            <button>Check out</button>
-
-            {/* Features e Descrição */}
+            <button id="check">Check out</button>
             <p className="features">Features</p>
             <ul>
               <li className="plantFeatures">{plant.features}</li>
@@ -82,7 +76,7 @@ const Products = () => {
               <p className="plantDescriptions">{plant.description}</p>
             </div>
           </div>
-        </div>
+        </>
       )}
     </div>
   );
